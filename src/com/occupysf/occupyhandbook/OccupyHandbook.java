c34203ef654f559c81188caea71250b014021946
Enrique Gavidia <oblique63@gmail.com>
2011-10-16 20:18:50 -0700

    first _real_ commit

---
+++ src/com/occupysf/occupyhandbook/OccupyHandbook.java
@@ -0,0 +1,13 @@
+package com.occupysf.occupyhandbook;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class OccupyHandbook extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
